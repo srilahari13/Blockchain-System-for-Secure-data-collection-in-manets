44c3ae1b2eada6f08363cb24c39c289dc4484500
Lahari <usrilahari@gmail.com>
2023-03-17 11:45:54 +0530

    updated logic for broadcasting

--- BroadCastingPeer.py
+++ BroadCastingPeer.py
@@ -2,13 +2,13 @@ import DataGeneration as DG
 import time
 import socket
 
-ip=''
+ip='50.50.51.124'
 
 def broadCastData(data):
     print(data)
     s=socket.socket()
-    s.connect(ip,12345)
-    s.send(data)
+    s.connect((ip,12345))
+    s.send(data.encode('utf-8'))
     s.close()
     time.sleep(4)
 
--- ReceivingPeer.py
+++ ReceivingPeer.py
@@ -1,12 +1,12 @@
 import socket
 
-ip=''
+ip='50.50.51.124'
 s=socket.socket()
-s.bind(ip,12345)
+s.bind((ip,12345))
 s.listen(5)
 
 while True:
     obj,addr=s.accept()
     print('Incoming Connection from BroadCast Peer',addr)
-    tmpData=s.recv(1024)
+    tmpData=obj.recv(1024)
     print(tmpData)
